96d84dcf68aa0055fb58b2a9e5546a44d093d360
Kadi Uday <udaykadi45@gmail.com>
2025-09-28 13:03:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import "./index.css"
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-blue-600'>hello world</h1>
+      <h2 className='text-4xl'>react project</h2>
+      <h3 className='font-bold bg-green-700'>Movie Review & Rating Platform</h3>
+    </>
+  )
+}
+
+export default App
